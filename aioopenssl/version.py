95668eb30fa0c8d0fd985ed18f2fcd27db6e21c4
Jonas Wielicki <j.wielicki@sotecware.net>
2017-02-26 16:26:37 +0100

    Bump version number to 0.3.0-a0

--- aioopenssl/version.py
+++ aioopenssl/version.py
@@ -1,4 +1,4 @@
-version_info = (0, 2, 0, "a0")
+version_info = (0, 3, 0, "a0")
 
 __version__ = ".".join(map(str, version_info[:3])) + ("-"+version_info[3] if
                                                       version_info[3] else "")
