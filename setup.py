059c5e8e08f5048833254662001cf1130c9d8931
Jonas Schäfer <j.wielicki@sotecware.net>
2020-04-10 12:44:26 +0200

    Announce Python 3.8 support

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Communications :: Chat",
     ],
     keywords="openssl asyncio library transport starttls",
