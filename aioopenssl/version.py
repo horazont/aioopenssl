9e9f5b288b9fecbd773ed43986f66fe393f16e3f
Jonas Schäfer <j.wielicki@sotecware.net>
2022-05-17 21:09:33 +0200

    Bump version to 0.7.0-a0

--- aioopenssl/version.py
+++ aioopenssl/version.py
@@ -1,4 +1,4 @@
-version_info = (0, 6, 0, "a0")
+version_info = (0, 7, 0, "a0")
 
 __version__ = ".".join(map(str, version_info[:3])) + (
     "-"+version_info[3] if version_info[3] is not None else ""  # type:ignore
